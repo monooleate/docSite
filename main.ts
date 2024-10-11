802e460db844ef288cf50fa3fcfb83be17819023
János Mészáros <taskony@gmail.com>
2024-10-11 10:23:32 +0200

    Initial commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { App, fsRoutes, staticFiles, trailingSlashes } from "fresh";
+
+export const app = new App({ root: import.meta.url })
+  .use(staticFiles())
+  .use(trailingSlashes("never"));
+
+await fsRoutes(app, {
+  loadIsland: (path) => import(`./islands/${path}`),
+  loadRoute: (path) => import(`./routes/${path}`),
+});
+
+if (import.meta.main) {
+  await app.listen();
+}
